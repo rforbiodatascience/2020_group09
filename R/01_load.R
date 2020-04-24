f0db219b519f413efe318a1dc8db5481d0365a0c
Laietam <s182542student.dtu.dk>
2020-04-24 15:28:06 +0000

    Load TCGA datasets correctly

--- R/01_load.R
+++ R/01_load.R
@@ -16,12 +16,13 @@ source(file = "R/99_project_functions.R")
 prostate_raw <- read.dta("data/_raw/prostate.dta")
 TCGA_prostate_raw <- read.table("data/_raw/TCGA-PRAD.GDC_phenotype.tsv", sep = "\t", header = T, quote = "", na.strings=c("","NA"))
 TCGA_prostate_survival_raw <- read.table("data/_raw/TCGA-PRAD.survival.tsv", sep = "\t", header = T, quote = "", na.strings=c("","NA"))
+#TCGA data already in tibble format 
 
 # Write data
 # ------------------------------------------------------------------------------
 write_tsv(x = prostate_raw,
           path = "data/01_prostate.tsv")
-write_tsv(x = prostate_raw,
+write_tsv(x = TCGA_prostate_raw,
           path = "data/01_TCGA_prostate.tsv")
-write_tsv(x = prostate_raw,
+write_tsv(x = TCGA_prostate_survival_raw,
           path = "data/01_TCGA_prostate_survival.tsv")
