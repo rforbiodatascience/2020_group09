601a59b97b2cbd6ebe35ed82a297a8ee1467c85d
giorgia.zoni <you@example.com>
2020-04-25 10:02:46 +0200

    Load and Clean scripts updated

--- R/01_load.R
+++ R/01_load.R
@@ -4,8 +4,8 @@ rm(list = ls())
 
 # Load libraries
 # ------------------------------------------------------------------------------
-library("tidyverse")
-library("foreign")
+library(tidyverse)
+library(foreign)
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -16,7 +16,16 @@ source(file = "R/99_project_functions.R")
 prostate_raw <- read.dta("data/_raw/prostate.dta")
 TCGA_prostate_raw <- read.table("data/_raw/TCGA-PRAD.GDC_phenotype.tsv", sep = "\t", header = T, quote = "", na.strings=c("","NA"))
 TCGA_prostate_survival_raw <- read.table("data/_raw/TCGA-PRAD.survival.tsv", sep = "\t", header = T, quote = "", na.strings=c("","NA"))
-#TCGA data already in tibble format 
+
+#Wrangle data
+#-------------------------------------------------------------------------------
+
+prostate_raw <- prostate_raw %>% 
+  as_tibble()
+TCGA_prostate_raw <- TCGA_prostate_raw %>% 
+  as_tibble()
+TCGA_prostate_survival_raw <- TCGA_prostate_survival_raw %>% 
+  as_tibble()
 
 # Write data
 # ------------------------------------------------------------------------------
--- R/02_clean.R
+++ R/02_clean.R
@@ -4,12 +4,12 @@ rm(list = ls())
 
 # Load libraries
 # ------------------------------------------------------------------------------
-library("tidyverse")
-library("dplyr")
-library("stringr")
-library("chron")
-library("lubridate")
-library("haven")
+library(tidyverse)
+library(dplyr)
+library(stringr)
+library(chron)
+library(lubridate)
+library(haven)
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -28,15 +28,14 @@ TCGA_prostate_survival_raw <- read_tsv(file = "data/01_TCGA_prostate_survival.ts
 # changing column names
 prostate <- prostate_raw %>% 
   mutate("Dataset" = "prostate") #column which identifies the dataset (it will be useful later)
-col_names <- c("Patient_ID", "Stage", "Estrogen(mg)", 
+
+col_names <- c("Patient_ID", "Stage", "Estrogen_mg", 
                "Months_FU", "Status", "Age", "Weight_index",
                "Activity_level", "CV_history", "SBP", "DBP",
                "EKG", "Serum_HG", "Tumour_size", "SG", "AP",
                "Bone_metastases", "Date_on_study","Dataset")
 
 colnames(prostate) <- col_names
-
-prostate <- as_tibble(prostate)
 
 #removing the rows which contain at least one NA
 prostate_data <- prostate %>%
@@ -45,12 +44,12 @@ prostate_data <- prostate %>%
 # split the estrogen (mg) col into two cols + deletion of the Estr col, 
 # since it contains only the word "estrogen"
 prostate_data <- prostate_data %>%
-  mutate('Estrogen(mg)' =str_replace(string = "Estrogen(mg)", pattern = "placebo", replacement = "0.0")) %>%  
-  separate(data = ., col = 'Estrogen(mg)', into = c("Estrogen(mg)", "Estr"), sep = "mg") #%>%
-  select(-'Estr')
+  mutate(Estrogen_mg = str_replace(string = Estrogen_mg, pattern = "placebo", replacement = "0.0")) %>%  
+  separate(data = ., col = Estrogen_mg, into = c("Estrogen_mg", "Estr"), sep = "mg") %>%
+  select(-Estr)
 
 prostate_data <- prostate_data %>%
-  mutate('Estrogen(mg)'= as.numeric(x = 'Estrogen(mg)', digits = 2))
+  mutate(Estrogen_mg = as.numeric(x = Estrogen_mg, digits = 2))
 
 
 #Transfom the data variable in a more readable version
@@ -81,6 +80,9 @@ prostate_data <- prostate_data %>%
                               Status == "dead - unspecified non-ca" ~ 8,
                               Status == "dead - respiratory disease" ~ 9))
 
+#prostate_data <- prostate_data %>% 
+#  mutate("Status" = as.integer(prostate_data$Status) -1) 
+#example of faster but non tidy code
 
 ## Working in the TCGA datasets
 
@@ -104,7 +106,6 @@ TCGA_prostate_tot <- left_join(x = TCGA_prostate_raw, y = TCGA_prostate_survival
 # bone_metastasis       bone_scan_results --> retain only normal + abnormal == 0, prostate cancer == 1, while equivocal == NA. 
 # monts of FU           OS.time --> those are days, we have to transform them in months
 # SG correlates with gleason score, primary_pattern and secondary_pattern                   
-# AP correlates with gleason score --> find a way to transform one into the other  ????????
   
 # Select the interesting columns, change names if necessary (Patient_ID),
 # and add data that can be extracted from the existing columns (SG)
@@ -114,38 +115,36 @@ TCGA_prostate_def <- TCGA_prostate_tot %>%
                         gleason_score==7 & primary_pattern == 4 ~  gleason_score+3,
                         gleason_score == 8 ~ gleason_score+4,
                         gleason_score>=9 ~ gleason_score+5)) %>% 
-  select("Patient_ID" = patient_id, age_at_initial_pathologic_diagnosis, bone_scan_results, OS.time, Dataset, SG) 
+  select("Patient_ID" = patient_id, age_at_initial_pathologic_diagnosis, bone_scan_results, OS.time, SG, Dataset) 
   
 # Remove duplicated rows
 TCGA_prostate_def <- TCGA_prostate_def %>%
   unique()
 
-# Transform data format to alow join
-prostate_data %>%
-  mutate(Patient_ID = as_factor(Patient_ID))
-TCGA_prostate_def %>% 
-  mutate(Patient_ID = as_factor(Patient_ID))
+# Transform data format to allow join
+prostate_data <- prostate_data %>%
+  mutate(Patient_ID = as.character(Patient_ID))
 
 #Convert the content of the OS.time column from days to months. 
 TCGA_prostate_def <- TCGA_prostate_def %>% 
   mutate("Months_FU" = round(OS.time/30, digits = 0), OS.time = NULL) %>% 
-  select(Patient_ID, age_at_initial_pathologic_diagnosis, Months_FU, bone_scan_results, Dataset, SG) 
+  select(Patient_ID, age_at_initial_pathologic_diagnosis, Months_FU, bone_scan_results, SG, Dataset) 
 
 TCGA_prostate_def <- TCGA_prostate_def %>% 
   mutate("Bone_metastases" = (case_when(
     bone_scan_results == "Normal (no evidence of prostate cancer) [cM0]" ~ 0, 
     bone_scan_results == "Abnormal (not related to prostate cancer)" ~ 0, 
-    bone_scan_results == "Prostate Cancer Metastases Present [cM1b]" ~ 1))) 
+    bone_scan_results == "Prostate Cancer Metastases Present [cM1b]" ~ 1)), 
+    bone_scan_results = NULL) 
 
 TCGA_prostate_final <- TCGA_prostate_def %>%
-  select(Patient_ID, age_at_initial_pathologic_diagnosis, Months_FU, Bone_metastases, Dataset, SG)
+  select(Patient_ID, age_at_initial_pathologic_diagnosis, Months_FU, Bone_metastases, SG, Dataset)
 
 
-colnames(TCGA_prostate_final) <- c("Patient_ID", "Age", "Months_FU", "Bone_metastases", "Dataset", "SG")
+colnames(TCGA_prostate_final) <- c("Patient_ID", "Age", "Months_FU", "Bone_metastases", "SG", "Dataset")
 
-prostate_tidy <- full_join(x = prostate_data, y = TCGA_prostate_final, by = c("Patient_ID", "Age", "Months_FU", "Dataset", "Bone_metastases", "SG")) %>% 
+prostate_tidy <- full_join(x = prostate_data, y = TCGA_prostate_final, by = c("Patient_ID", "Age", "Months_FU", "Bone_metastases", "SG", "Dataset")) %>% 
   select(Patient_ID, Age, Months_FU, Bone_metastases, everything())
-
 
 # Write data
 # ------------------------------------------------------------------------------
