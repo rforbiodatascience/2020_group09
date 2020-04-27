9bcc53233a351f7dbb8649484be14ba5b69b6b3b
Laietam <s182542student.dtu.dk>
2020-04-27 21:09:27 +0000

    Change to variables to lowercase letters

--- R/01_load.R
+++ R/01_load.R
@@ -4,8 +4,8 @@ rm(list = ls())
 
 # Load libraries
 # ------------------------------------------------------------------------------
-library(tidyverse)
-library(foreign)
+library("tidyverse")
+library("foreign")
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -14,24 +14,15 @@ source(file = "R/99_project_functions.R")
 # Load data
 # ------------------------------------------------------------------------------
 prostate_raw <- read.dta("data/_raw/prostate.dta")
-TCGA_prostate_raw <- read.table("data/_raw/TCGA-PRAD.GDC_phenotype.tsv", sep = "\t", header = T, quote = "", na.strings=c("","NA"))
-TCGA_prostate_survival_raw <- read.table("data/_raw/TCGA-PRAD.survival.tsv", sep = "\t", header = T, quote = "", na.strings=c("","NA"))
-
-#Wrangle data
-#-------------------------------------------------------------------------------
-
-prostate_raw <- prostate_raw %>% 
-  as_tibble()
-TCGA_prostate_raw <- TCGA_prostate_raw %>% 
-  as_tibble()
-TCGA_prostate_survival_raw <- TCGA_prostate_survival_raw %>% 
-  as_tibble()
+tcga_prostate_raw <- read.table("data/_raw/TCGA-PRAD.GDC_phenotype.tsv", sep = "\t", header = T, quote = "", na.strings=c("","NA"))
+tcga_prostate_survival_raw <- read.table("data/_raw/TCGA-PRAD.survival.tsv", sep = "\t", header = T, quote = "", na.strings=c("","NA"))
+#TCGA data already in tibble format 
 
 # Write data
 # ------------------------------------------------------------------------------
 write_tsv(x = prostate_raw,
           path = "data/01_prostate.tsv")
-write_tsv(x = TCGA_prostate_raw,
+write_tsv(x = tcga_prostate_raw,
           path = "data/01_TCGA_prostate.tsv")
-write_tsv(x = TCGA_prostate_survival_raw,
+write_tsv(x = tcga_prostate_survival_raw,
           path = "data/01_TCGA_prostate_survival.tsv")
