2e3e8b0a6f908b8acc11bd761faa98a79113dd07
Laietam <s182542student.dtu.dk>
2020-04-28 00:24:27 +0000

    It's not finished

--- R/03_augment.R
+++ R/03_augment.R
@@ -12,47 +12,126 @@ source(file = "R/99_project_functions.R")
 
 # Load data
 # ------------------------------------------------------------------------------
-prostate_data <- read_tsv(file = "data/02_prostate_data_clean.tsv")
+prostate_clean <- read_tsv(file = "data/02_prostate_clean.tsv")
+tcga_prostate_clean <- read_tsv(file = "data/02_tcga_prostate_clean.tsv")
+tcga_prostate_survival_clean <- read_tsv(file = "data/02_tcga_survival_prostate_clean.tsv")
 
 # Wrangle data
 # ------------------------------------------------------------------------------
 
+# Augment prostate data
+
+# Add column dataset
+prostate_data <- prostate_clean %>% 
+  mutate("dataset" = "prostate")
+
 #one hot encoding factors (which are actually characters!)
-prostate_one_hot_EKG <- prostate_data %>% 
-  group_by(EKG) %>%
+prostate_one_hot <- prostate_data %>% 
+  group_by(ekg) %>%
   mutate(count = 1) %>%
-  pivot_wider(names_from = EKG, values_from = count, names_prefix = "EKG_", values_fill = list(count = 0))
+  pivot_wider(names_from = ekg, values_from = count, 
+              names_prefix = "ekg_", values_fill = list(count = 0))
 
-prostate_one_hot_EKG_activity <- prostate_one_hot_EKG %>% 
-  group_by(Activity_level) %>%
+prostate_one_hot <- prostate_one_hot %>% 
+  group_by(activity_level) %>%
   mutate(count = 1) %>%
-  pivot_wider(names_from = Activity_level, values_from = count, names_prefix = "Activity_", values_fill = list(count = 0))
+  pivot_wider(names_from = activity_level, 
+              values_from = count, 
+              names_prefix = "activity_", 
+              values_fill = list(count = 0))
 
-prostate_one_hot_factors <- prostate_one_hot_EKG_activity %>% 
-  group_by(Status) %>%
+prostate_one_hot_factors <- prostate_one_hot %>% 
+  group_by(status) %>%
   mutate(count = 1) %>%
-  pivot_wider(names_from = Status, values_from = count, names_prefix = "Status_", values_fill = list(count = 0))
+  pivot_wider(names_from = status, 
+              values_from = count, 
+              names_prefix = "status_", 
+              values_fill = list(count = 0))
 
 #one hot with three status classes: alive, dead - prostata ca, dead_other 
 prostate_one_hot_status3 <- prostate_one_hot_factors %>% 
-  mutate(`Status_dead other` = `Status_dead - cerebrovascular` | 
-           `Status_dead - heart or vascular` | 
-           `Status_dead - pulmonary embolus` | 
-           `Status_dead - other ca` |
-           `Status_dead - other specific non-ca` | 
-           `Status_dead - unspecified non-ca` | 
-           `Status_dead - respiratory disease` | 
-           `Status_dead - unknown cause`) %>% 
-  mutate(`Status_dead other`= as.numeric(`Status_dead other`)) %>% 
-  select(-`Status_dead - cerebrovascular`, 
-         - `Status_dead - heart or vascular`, 
-         - `Status_dead - pulmonary embolus`, 
-         - `Status_dead - other ca`,
-         - `Status_dead - other specific non-ca`,  
-         - `Status_dead - unspecified non-ca`,
-         - `Status_dead - respiratory disease`, 
-         - `Status_dead - unknown cause`)
-  
+  mutate(`status_dead other` = `status_dead - cerebrovascular` | 
+           `status_dead - heart or vascular` | 
+           `status_dead - pulmonary embolus` | 
+           `status_dead - other ca` |
+           `status_dead - other specific non-ca` | 
+           `status_dead - unspecified non-ca` | 
+           `status_dead - respiratory disease` | 
+           `status_dead - unknown cause`) %>% 
+  mutate(`status_dead other`= as.numeric(`status_dead other`)) %>% 
+  select(-`status_dead - cerebrovascular`, 
+         - `status_dead - heart or vascular`, 
+         - `status_dead - pulmonary embolus`, 
+         - `status_dead - other ca`,
+         - `status_dead - other specific non-ca`,  
+         - `status_dead - unspecified non-ca`,
+         - `status_dead - respiratory disease`, 
+         - `status_dead - unknown cause`)
+
+# Augment tcga datasets
+
+# Convert the content of the OS.time column from days to months., 
+tcga_prostate_survival_data <- tcga_prostate_survival_clean %>% 
+  mutate("months_fu" = round(os_time/30, digits = 0), os_time = NULL) 
+
+# Binarize bone metastases results
+tcga_prostate_data <- tcga_prostate_clean %>% 
+  mutate("bone_metastases" = (case_when(
+    bone_scan_results == "Normal (no evidence of prostate cancer) [cM0]" ~ 0, 
+    bone_scan_results == "Abnormal (not related to prostate cancer)" ~ 0, 
+    bone_scan_results == "Prostate Cancer Metastases Present [cM1b]" ~ 1)), 
+    bone_scan_results = NULL)
+
+# Obtain SG value from gleason score and primary and secondary pattern
+tcga_prostate_data <- tcga_prostate_data %>% 
+  mutate('sg'=case_when(gleason_score<=6 ~ gleason_score+1,
+                        gleason_score==7 & primary_pattern == 3 ~  gleason_score+2,
+                        gleason_score==7 & primary_pattern == 4 ~  gleason_score+3,
+                        gleason_score == 8 ~ gleason_score+4,
+                        gleason_score>=9 ~ gleason_score+5))
+
+# Select the useful data
+tcga_prostate_data_subset <- tcga_prostate_data %>%
+  select(sample_id, age, bone_metastases, sg)
+
+tcga_prostate_survival_data_subset <- tcga_prostate_survival_data %>%
+  select(sample_id, months_fu)
+# Check the sample_id are the same in both tibbles
+# anti_join(x = tcga_prostate_raw, y = tcga_prostate_raw, by = "sample_id")
+
+# Inner-join: join the phenotipe with the survival rates using the Sample ID
+# as key column. Only takes what is in both datasets
+# Add Dataset column to identify the origin of the data
+tcga_prostate_subset_tot <- inner_join(x = tcga_prostate_data_subset,
+                                y = tcga_prostate_survival_data_subset, 
+                                by = "sample_id") %>% 
+  mutate("dataset" = "tcga") %>% 
+  rename("patient_id" = "sample_id")
+
+#Check !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Remove duplicated rows
+#tcga_prostate_tot <- tcga_prostate_tot %>%
+#duplicated() #unique()
+#unique(tcga_prostate_tot) --> There are no duplicateds
+
+ 
+# Common columns in both datasets:
+
+# prostate data          TCGA
+# Patient id            patient_id
+# Age                   age_at_initial_pathologic_diagnosis
+# bone_metastasis       bone_scan_results --> retain only normal + abnormal == 0, prostate cancer == 1, while equivocal == NA. 
+# monts of FU           OS.time --> those are days, we have to transform them in months
+# sg correlates with gleason score, primary_pattern and secondary_pattern                   
+
+prostate_data_subset <- prostate_data %>% 
+  select(patient_id, age, bone_metastases, months_fu,sg, dataset) %>% 
+  mutate(patient_id = as.character(patient_id))
+
+# Select the interesting columns, change names if necessary (patient_id),
+# and add data that can be extracted from the existing columns (sg)
+prostate_join <- bind_rows(x = prostate_data_subset,
+                           y = tcga_prostate_subset_tot)
 # ------------------------------------------------------------------------------
 # write_tsv(x = prostate_data_status_in_numbers,
 #           path = "data/03_prostate_status_in_numbers.tsv")
