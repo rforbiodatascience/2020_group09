17b83bffb78d85c157e4cef26281e88fd69a73e3
mbj <maja_jager@hotmail.com>
2020-04-30 14:27:31 +0000

    one hot function

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -1,5 +1,15 @@
 # Define project functions
 # ------------------------------------------------------------------------------
+
+one_hot_encoder <- function(col_name, prefix){
+  group_by(col_name) %>%
+  mutate(count = 1) %>%
+  pivot_wider(names_from = col_name, 
+              values_from = count, 
+              names_prefix = prefix, 
+              values_fill = list(count = 0)) 
+}
+
 foo <- function(x){
   return(2*x)
 }
