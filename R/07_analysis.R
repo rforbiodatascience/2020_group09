799bb8e24b4b2d95a93d0b0b4117996163cd8352
Laietam <s182542@student.dtu.dk>
2020-05-05 14:19:16 +0000

    Regression working

--- R/07_analysis.R
+++ R/07_analysis.R
@@ -27,13 +27,13 @@ source(file = 'R/99_project_functions.R')
 joined_data = read_tsv(file = "data/03_prostate_and_tcga_joined")
 
 regr_data <- joined_data %>% 
-  select(months_fu, age, sg, bone_metastases, status_alive,
+  select(months_fu, status_alive,  age, sg, bone_metastases,
          status_dead_prostatic_ca, status_dead_other, dataset)
-
+# 
 # Prepare data
 # ------------------------------------------------------------------------------
 ## select 80% of the sample size for training
-smp_size <- floor(0.8 * nrow(regr_data))
+smp_size <- floor(0.9 * nrow(regr_data))
 set.seed(42)
 train_ind <- sample(seq_len(nrow(regr_data)), size = smp_size)
 
@@ -57,33 +57,16 @@ test_y <- test  %>%
 test_x <- test %>% 
   select(-c(months_fu))%>% 
   as.matrix()  
-#head(test_x)
 
-#""""""""
-
-train_y = regr_data %>%
-  filter(row_number() %in% train_ind)%>% 
-  pull(months_fu)
-train_x = regr_data %>%
-  filter(row_number() %in% train_ind)%>% 
-  data.frame()
-
-test_x <- test %>% 
-  select(-c(months_fu))%>%
-  data.frame()
-train_x <- train %>% 
-  select(-c(months_fu))%>%
-  data.frame()
-  #pull(months_fu)
 # Define ANN model
 # ------------------------------------------------------------------------------
 
 # Set hyperparameters
-n_hidden_1 = 32
+n_hidden_1 = 128
 h1_activate = 'relu'
-#drop_out_1 = 0.4
-# n_hidden_2 = 90
-# h2_activate = 'relu'
+drop_out_1 = 0.4
+#n_hidden_2 = 128
+#h2_activate = 'relu'
 # drop_out_2 = 0.3
 # n_hidden_3 = 45
 # h3_activate = 'relu'
@@ -93,16 +76,17 @@ h1_activate = 'relu'
 # drop_out_4 = 0.1
 n_output   = 1
 o_ativate  = 'relu'
-n_epochs = 15
+n_epochs = 100
 batch_size = 50
 loss_func = 'mean_squared_error'
-learn_rate = 0.001
+learn_rate = 0.005
 
 # Set architecture
 model = keras_model_sequential() %>% 
   layer_dense(units = n_hidden_1, 
-              activation = h1_activate, input_shape = 7) %>% 
-  # layer_dropout(rate = drop_out_1) %>% 
+              activation = h1_activate, input_shape = 7,
+              kernel_regularizer =  regularizer_l2(0.001)) %>% 
+  layer_dropout(rate = drop_out_1) %>% 
   # layer_dense(units = n_hidden_2, activation = h2_activate) %>%
   # layer_dropout(rate = drop_out_2) %>%
   # layer_dense(units = n_hidden_3, activation = h3_activate) %>%
@@ -114,8 +98,8 @@ model = keras_model_sequential() %>%
 # Compile model
 model %>%
   compile(loss = loss_func,
-          optimizer = optimizer_adam(lr = learn_rate),
-          metrics = c('accuracy')
+          optimizer = optimizer_rmsprop(lr = learn_rate),
+          metrics = c('mae')
   )
 
 # View model
@@ -128,31 +112,42 @@ history = model %>%
       y = train_y,
       epochs = n_epochs,
       batch_size = batch_size,
-      validation_split = 0.8)
-      #validation_data = (test_x, test_y))
+      #validation_split = 0.8)
+      validation_data = list(test_x, test_y))
 
 # Evaluate model
 # ------------------------------------------------------------------------------
-perf_test = model %>% evaluate(X_test, y_test)
-acc_test = perf_test %>% pluck('acc') %>% round(3) * 100
-perf_train = model %>% evaluate(X_train, y_train)
-acc_train = perf_train %>% pluck('acc') %>% round(3) * 100
+# Calculate performance on test data
+y_test_true = test_y
+y_test_pred = model %>% predict(test_x) %>% as.vector
+pcc_test = round(cor(y_test_pred, y_test_true, method = "pearson"), 3)
+
+# Calculate performance on training data
+y_train_true = train_y
+y_train_pred = model %>% predict(train_x) %>% as.vector
+perf_test
+
+perf_test = model %>% evaluate(test_x, test_y)
+mae_test = perf_test %>% pluck('mean_absolute_error') %>% round(3) #* 100
+
+perf_train = model %>% evaluate(train_x, train_x)
+mae_train = perf_train %>% pluck('mean_absolute_error') %>% round(3)# * 100
 results = bind_rows(
-  tibble(y_true = y_test %>%
+  tibble(y_true = test_y %>%
            apply(1, function(x){ return( which(x==1) - 1) }) %>%
            factor,
          y_pred = model %>%
-           predict_classes(X_test) %>%
+           predict_classes(test_x) %>%
            factor,
          Correct = ifelse(y_true == y_pred ,"yes", "no") %>%
            factor,
          data_type = 'test')
   ,
-  tibble(y_true = y_train %>%
+  tibble(y_true = train_y %>%
            apply(1, function(x){ return( which(x==1) - 1) }) %>%
            factor,
          y_pred = model %>%
-           predict_classes(X_train) %>%
+           predict_classes(train_x) %>%
            factor,
          Correct = ifelse(y_true == y_pred ,"yes", "no") %>%
            factor,
