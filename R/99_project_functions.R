c5ff05bd7586ab4d8efdba3711e1cdf0b03da2a9
giorgia.zoni <you@example.com>
2020-05-07 11:26:08 +0200

    fixing_hot_encoder_function

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -1,26 +1,16 @@
 # Define project functions
 # ------------------------------------------------------------------------------
 
-one_hot_encoder <- function(dataset, prefix, ...){
+one_hot_encoder <- function(dataset, prefix, colname){
+  
+  
   dataset_one_hot_encoded <- dataset %>%
-                              group_by(...) %>%
                               mutate(count = 1) %>%
-                              pivot_wider(names_from = ..., 
+                              pivot_wider(names_from = colname, 
                                           values_from = count, 
                               names_prefix = prefix, 
                               values_fill = list(count = 0)) 
   return(dataset_one_hot_encoded)
-}
-
-# function to change factor columns into new columns for each level denoted 
-# by 0,1, 0 for abscent 1 for present - one hot encoding.  
-one_hot_encoder <- function(col_name, prefix){
-  group_by(col_name) %>%
-  mutate(count = 1) %>%
-  pivot_wider(names_from = col_name, 
-              values_from = count, 
-              names_prefix = prefix, 
-              values_fill = list(count = 0)) 
 }
 
 # ----------------------------------------------------------------------------
