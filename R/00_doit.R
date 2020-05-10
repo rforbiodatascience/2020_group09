0c5d3b71951334793f55232311747cc196cc61e1
Laietam <s182542@student.dtu.dk>
2020-05-10 17:26:27 +0000

    Change names

--- R/00_doit.R
+++ R/00_doit.R
@@ -3,5 +3,9 @@
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
-source(file = "R/04_analysis_i.R")
-source(file = "R/05_analysis_ii.R")
+source(file = "R/04_analysis_and_plot.R")
+source(file = "R/05_pca_and_clustering.R")
+source(file = "R/06_linear_regression.R")
+source(file = "R/07_ann_regression.R")
+source(file = "R/08_ann_classification.R")
+
--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -6,7 +6,7 @@ one_hot_encoder <- function(dataset, prefix, colname){
   
   dataset_one_hot_encoded <- dataset %>%
                               mutate(count = 1) %>%
-                              pivot_wider(names_from = colname, 
+                              pivot_wider(names_from = all_of(colname), 
                                           values_from = count, 
                               names_prefix = prefix, 
                               values_fill = list(count = 0)) 
