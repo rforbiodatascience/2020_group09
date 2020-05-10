06ac6e58473b57697c9130d64885571b94eca29a
giorgia.zoni <you@example.com>
2020-05-10 16:33:06 +0200

    presentation and ggplot updates

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -8,6 +8,8 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 library(ggthemes)
+library(reshape2)
+library(cowplot)
 require(gridExtra)
 
 # Define functions
@@ -57,21 +59,27 @@ for (i in 1:length(data_to_plot)
   col <- data_to_plot[[i]]
   x_lab <- colnames(data_to_plot)[i]
   
-  png_title <- sprintf("Histogram and boxplot of %s", x_lab)
-  
   barplot_i <- ggplot(data = data_to_plot, mapping = aes(x = col)) +
     geom_histogram(aes(y = ..density..), 
                    binwidth = function(x)2*IQR(x)/length(x)^(1/3)) +
     geom_density(color = "red") +
-    labs(x = x_lab)
+    labs(x = x_lab) +
+    theme(axis.title = element_text(size = 16))
   
   boxplot_i <- ggplot(data = data_to_plot, mapping = aes(y = col)) +
     geom_boxplot() +
-    labs(x = x_lab)
+    labs(x = x_lab) +
+    theme(axis.title = element_text(size = 16))
   
+  png_title <- ggdraw() + 
+    draw_label(paste("Histogram and boxplot of ", x_lab, sep = ""), 
+               size = 22) 
   
-  histogram_and_box_plot <- grid.arrange(barplot_i, 
-                                         boxplot_i, ncol = 2, top = png_title) 
+  plot_row <- plot_grid(barplot_i, boxplot_i, ncol = 2)
+  histogram_and_box_plot <- plot_grid(png_title, 
+                                      plot_row, 
+                                      ncol = 1, rel_heights = c(0.1, 1))
+    
   
   plot_list[[i]] <- histogram_and_box_plot
   names(plot_list)[i] <- paste("histogram_and_boxplot_of_", x_lab, sep = "")
@@ -85,24 +93,32 @@ months_vs_all <- data_to_plot_long %>%
                              ifelse(cat_status == 0, "Alive", "Death from other causes") ))) +
   geom_point() +
   facet_wrap(~vars, nrow = 5, scales = "free") +
-  labs(x = "", colour = "Status", title = 'Months of follow-up plotted over the other continous variables') +
+  theme(strip.text.x = element_text(size = 14)) +
+  labs(x = "", 
+       colour = "Status", 
+       title = 'Months of follow-up plotted over the other continous variables') +
+  theme(plot.title = element_text(size = 22), 
+        legend.title = element_text(size = 18),
+        legend.text = element_text(size = 14), 
+        ) +
   scale_color_colorblind()
  
 corr_matrix <- ggplot(data = data_to_corr, aes(Var2, Var1, fill = value)) +
   geom_tile(color = "gray") +
-  geom_text(aes(Var2, Var1, label = value), color = "black", size = 2) +
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
   scale_fill_gradient2(low = "#0072B2", high = "#D55E00", mid = "white", 
                        midpoint = 0, limit = c(-1,1), space = "Lab"  , 
                        name="Correlation", na.value = 'white') +
-  labs(title = "Correlation Matrix") +
+  #labs(title = "Correlation Matrix") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 8, hjust = 1), 
+                                   size = 10, hjust = 1), 
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         panel.grid.major = element_blank(),
         panel.border = element_blank(),
-        panel.background = element_blank())
+        panel.background = element_blank(),
+        plot.title = element_text(size = 22))
 
 
 # Write data
